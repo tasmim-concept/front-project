29bbf1d72c2042964ba1525dc2519319403e8142
tasmim-concept <tasmim.concept@gmail.com>
2020-07-02 01:45:47 +0200

    Création de la structure
    Création de la structure du template et ajout d'une description explicative sur son utilisation.

---
+++ README.md
@@ -0,0 +1,98 @@
+# Description du template
+
+Template de base pour démarrer un projet front avec une pré-configuration de webpack pour l'utilisation de React et Sass.
+<br>
+<br>Attention : le template utilise React Hot Loader ce qui peut ne pas convenir à tous les projets.
+
+## Installation du template
+
+1. Copier le dossier à l'endroit souhaitée ou cliquer sur "Use this template" et créer un nouveau projet.
+2. Ouvrir le terminal de commande sur le dossier front.
+3. Lancer la commande `npm i`.
+4. Lancer ensuite la commande `npm run start`.
+5. Vous pouvez enfin vous rendre dans le dossier front/src et commencer à développer.
+6. Lancer `npm run build` pour générer vos fichiers de production.
+
+## Organisation des dossiers :
+
+    front/
+        |-- package.json
+        |-- webpack.config.json
+        |-- .babelrc
+        |-- .browserslistrc
+        |-- .eslintrc
+        |-- src/
+            |-- css/
+                |-- style.scss
+            |-- js/
+                |-- index.jsx
+            |-- img/
+            |-- fonts/
+    public/
+        |-- assets/ (auto-generated)
+            |-- js/
+                |-- index.min.jsx
+            |-- css/
+                |-- index.min.css
+            |-- img/
+        |-- uploads/
+            |-- img/
+        |-- index.html
+
+
+## Dépendances pré-installées
+
+Les commandes ne sont pas à réécrire sauf si vous effacez le fichier package.json et souhaités personnaliser vos dépendances. Pour l'installation, reportez-vous à la section installation.
+
+### Dev-dependencies
+
+#### Webpack
+    npm i -D webpack
+    npm i -D webpack-cli
+    npm i -D webpack-dev-server
+
+#### CSS
+	npm i -D css-loader //comprend le css
+    npm i -D sass-loader //convertis le sass/scss en css
+    npm i -D style-loader //écris le css dans le head
+    npm i -D postcss-loader //optimise le css
+    npm i -D mini-css-extract-plugin //écris le css dans un fichier css
+
+#### JS
+    npm i -D babel-loader
+    npm i -D @babel/preset-env
+    npm i -D @babel/preset-react
+    npm i -D react
+    npm i -D react-dom
+    npm i -D @hot-loader/react-dom
+    npm i -D react-hot-loader
+
+#### Fonts
+    npm i -D file-loader
+
+#### Images
+    npm i -D url-loader
+    npm i -D img-loader
+
+#### Optimisation
+    npm i -D clean-webpack-plugin
+    npm i -D eslint-loader (assure que le js est bien écrit)
+
+### Inter-Depedencies
+
+    npm i -D @babel/core
+    npm i -D eslint
+    npm i -D standard //eslint dependence
+    npm i -D babel-eslint
+    npm i -D imagemin
+    npm i -D imagemin-gifsicle
+    npm i -D imagemin-mozjpeg
+    npm i -D imagemin-pngquant
+    npm i -D imagemin-svgo
+    npm i -D autoprefixer
+    npm i -D cssnano
+    npm i -D node-sass
+
+### Depedencies
+
+    npm i jquery (optionnel parce que installé via les autres dépendences)
---
+++ front/webpack.config.js
@@ -0,0 +1,151 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const cssLoaders = [
+    {
+        loader: 'css-loader',
+        options: {
+            importLoaders: 1
+        }
+    },
+    {
+        loader: 'postcss-loader',
+        options: {
+            plugins: [
+                require('autoprefixer'),
+                require('cssnano')
+            ]
+        }
+    },
+    'sass-loader'
+]
+
+const config = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        index: ['./js/index.jsx', './css/style.scss'],
+        // admin: ['./js/admin.jsx'] //e.g. to add others files
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.json'],
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, '../public/assets'),
+        publicPath: 'assets/',
+        filename: 'js/[name].min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx?)$/,
+                exclude: '/(node_modules|bower_components)/',
+                use: ['react-hot-loader/webpack', 'babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.(woff2?|ttf|otf|eot)$/,
+                options: {
+                    outputPath: 'fonts'
+                },
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[hash:8].[ext]',
+                            outputPath: 'img'
+                        },
+                    },
+                    {
+                        loader: 'img-loader',
+                        options: {
+                            outputPath: 'img',
+                            enabled: true,
+                            plugins: [
+                                require('imagemin-gifsicle')({
+                                    interlaced: false
+                                }),
+                                require('imagemin-mozjpeg')({
+                                    progressive: true,
+                                    arithmetic: false
+                                }),
+                                require('imagemin-pngquant')({
+                                    floyd: 0.5,
+                                    speed: 2
+                                }),
+                                require('imagemin-svgo')({
+                                    plugins: [
+                                        { removeTitle: true },
+                                        { removeDesc: true },
+                                        { removeXMLNS: false },
+                                        { convertPathData: false }
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, '../public'),
+        overlay: false,
+        hot: true,
+        port: 8000,
+        historyApiFallback: {
+            rewrites: [
+                { from: /^\/$/, to: 'index.html' },
+            ]
+        }
+    },
+    devtool: 'eval-cheap-module-source-map'
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.plugins.push(new webpack.HotModuleReplacementPlugin());
+        config.module.rules.push({
+            test: /\.(sc|c|sa)ss$/i,
+            use: ['style-loader', ...cssLoaders]
+        })
+    }
+
+    if (argv.mode === 'production') {
+        config.module.rules.push({
+            test: /\.(sc|c|sa)ss$/i,
+            use: [ // order important
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                },
+                ...cssLoaders
+            ]
+        })
+        config.plugins.push(new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css'
+        }))
+        config.plugins.push(new CleanWebpackPlugin({
+            verbose: true,
+            cleanOnceBeforeBuildPatterns: ['**/*', '!uploads/**'],
+        }))
+    }
+
+    return config
+}
